d20e4431b4d6d6b3b83cee09e58a731cae672c2a
petterquinones <pedro.quinones@ias.com.co>
2023-01-26 14:06:43 -0500

    Creación y configuración del proyecto.

---
+++ src/main/java/com/apirest/ApiRestProyectoFinal/ApiRestProyectoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.ApiRestProyectoFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestProyectoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestProyectoFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apirest/ApiRestProyectoFinal/ApiRestProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.ApiRestProyectoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
